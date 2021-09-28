eeee86907e2c812508ae99a85279c6a979e3bf3b
fireflysmk <fireflysmk@gmail.com>
2021-09-28 18:21:09 +0300

    lesson1

---
+++ lesson1/src/main/java/Fruit.java
@@ -0,0 +1,18 @@
+public class Fruit {
+
+    private double weight;
+
+    public Fruit(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+
+}
